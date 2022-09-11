e832ae6fe90b8f370c694e42af5a538931280d28
eby saju <ebysaju123@gmail.com>
2022-09-11 02:58:57 -0400

    retrieved profile image for users

--- lib/userhomepage.dart
+++ lib/userhomepage.dart
@@ -4,41 +4,56 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-class userHomePage extends StatefulWidget
-{
-
+class userHomePage extends StatefulWidget {
   @override
-  _userHomeState createState()=> new _userHomeState();
-
+  _userHomeState createState() => new _userHomeState();
 }
-class _userHomeState extends State<userHomePage>{
+
+class _userHomeState extends State<userHomePage> {
   bool _pinned = true;
   bool _snap = false;
   bool _floating = false;
-   String username='';
-   void initState()
-   {
-     super.initState();
-     getusername();
-     Reference storageReference = FirebaseStorage.instance.ref().child("userData");
-     if (FirebaseAuth.instance.currentUser != null) {
-       print(FirebaseAuth.instance.currentUser?.uid);
-      final snapshot= FirebaseFirestore.instance.collection("userData").doc(FirebaseAuth.instance.currentUser?.uid).get();
-       print(snapshot);
-     }
-
-   }
+  String username = '';
+  String urlProfile='';
 
 
-  void getusername() async
-  {
-setState(() {
-  username=FirebaseAuth.instance.currentUser!.email!;
-});
+  void initState() {
+    super.initState();
+    getProfileUrl();
+    getusername();
+    /*Reference storageReference =
+        FirebaseStorage.instance.ref().child("userData");
+    if (FirebaseAuth.instance.currentUser != null) {
+      print(FirebaseAuth.instance.currentUser?.uid);
+      final snapshot = FirebaseFirestore.instance
+          .collection("userData")
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get();
+      print(snapshot);
+    }*/
+
+
+
+
+
   }
 
 
+  void getusername() async {
+    setState(() {
+      username = FirebaseAuth.instance.currentUser!.email!;
+    });
+  }
+  Future<void> getProfileUrl() async {
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance
+        .collection("userData")
+        .doc(FirebaseAuth.instance.currentUser?.uid)
+        .get();
 
+    setState(() {
+      urlProfile = snapshot.get('profilePhotoUrl');
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,16 +64,13 @@ setState(() {
             snap: _snap,
             floating: _floating,
             expandedHeight: 160.0,
-            flexibleSpace:  FlexibleSpaceBar(
-
+            flexibleSpace: FlexibleSpaceBar(
               title: Text('$username'),
-              background:Image.network("https://firebasestorage.googleapis.com/v0/b/room-sharing-4a199.appspot.com/o/test%2Faae17286-059a-4e63-8c32-51894be3e8a4%7D?alt=media&token=65745a5b-f823-4dab-a6d3-148d17f6b3c0",
-              fit: BoxFit.cover,),
+              background: urlProfile.isNotEmpty ? Image.network(
+                urlProfile,
+                fit: BoxFit.cover,
+              ):null
             ),
-
-
-
-
           ),
           const SliverToBoxAdapter(
             child: SizedBox(
@@ -70,7 +82,7 @@ setState(() {
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
+              (BuildContext context, int index) {
                 return Container(
                   color: index.isOdd ? Colors.white : Colors.black12,
                   height: 100.0,
@@ -123,7 +135,7 @@ setState(() {
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                   Text('$username'),
+                  Text('$username'),
                   Switch(
                     onChanged: (bool val) {
                       setState(() {
@@ -141,5 +153,4 @@ setState(() {
       ),
     );
   }
-
 }
